73c399248b9c11bdf89a1ace3948f820c1f7e045
rossedfort <rossedfort@gmail.com>
2015-12-14 16:54:46 -0700

    change oauth keys to production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :flickr, ENV["FLICKR_PUBLIC_KEY"], ENV["FLICKR_SECRET_KEY"], scope: "write"
+  provider :flickr, ENV["FLICKR_PUBLIC_KEY_PRO"], ENV["FLICKR_SECRET_KEY_PRO"], scope: "write"
 end
