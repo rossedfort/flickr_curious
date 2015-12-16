d1eff667a7e9b4536d10e4c85d3968a6a51e4913
rossedfort <rossedfort@gmail.com>
2015-12-15 20:50:50 -0700

    made buddy_icon dynamic to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :flickr_user, :first_album_photo, :get_photo
+  helper_method :current_user, :flickr_user, :first_album_photo, :get_photo, :user_info
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
