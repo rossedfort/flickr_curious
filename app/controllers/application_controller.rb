f4d5bec0fa14c4c9c149b02f6bf2eccdea6581ed
rossedfort <rossedfort@gmail.com>
2015-12-15 19:22:01 -0700

    fix helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :flickr_user, :first_album_photo
+  helper_method :current_user, :flickr_user, :first_album_photo, :get_photo
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
